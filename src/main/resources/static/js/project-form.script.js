5e162a7aa45887aada67ba78f4ec27a3614bf6d1
edneyRoldao <edneyroldao@gmail.com>
2024-10-28 03:33:51 -0300

    endDate and realEndDate form validation has been added

--- src/main/resources/static/js/project-form.script.js
+++ src/main/resources/static/js/project-form.script.js
@@ -1,7 +1,23 @@
 const projectFormScript = (() => {	
-	
-	document.addEventListener('DOMContentLoaded', function() {
-	    const choices = new Choices('#memberSelectId')
-	});
-	
+	validateInputDate()
+	membersSelectAutocompleteConfig()
+
+    function validateInputDate() {
+        const startDateInput = document.getElementById('project-start-date');
+        const expectedEndDateInput = document.getElementById('project-expected-end-date');
+        const actualEndDateInput = document.getElementById('project-actual-end-date');
+
+        startDateInput.addEventListener('change', function() {
+            const startDate = startDateInput.value;
+            expectedEndDateInput.min = startDate;
+            actualEndDateInput.min = startDate;
+        });
+    }
+
+    function membersSelectAutocompleteConfig() {
+        document.addEventListener('DOMContentLoaded', function() {
+            const choices = new Choices('#memberSelectId')
+        });
+    }
+
 })()
